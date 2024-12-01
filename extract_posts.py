0de6b8405600585ba59a97a6d20c0a4690274fba
Max Base <MaxBaseCode@Gmail.Com>
2024-12-02 00:11:49 +0330

    Update extract_posts.py

--- extract_posts.py
+++ extract_posts.py
@@ -1,11 +1,19 @@
+import os
 import requests
 import feedparser
 from datetime import datetime
 
-rss_url = 'https://jadi.net/feed/'
+RSS_URL = 'https://jadi.net/feed/'
+POSTS_DIR = 'posts'
+INDEX_FILE = 'index.html'
 
-response = requests.get(rss_url)
+if not os.path.exists(POSTS_DIR):
+    os.makedirs(POSTS_DIR)
+
+response = requests.get(RSS_URL)
 feed = feedparser.parse(response.content)
+
+post_links = []
 
 for entry in feed.entries:
     title = entry.title
@@ -26,8 +34,33 @@ for entry in feed.entries:
     </html>
     """
     
-    filename = f"post_{datetime.now().strftime('%Y%m%d%H%M%S')}_{title.replace(' ', '_')}.html"
+    filename = f"{POSTS_DIR}/post_{datetime.now().strftime('%Y%m%d%H%M%S')}_{title.replace(' ', '_')}.html"
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(html_content)
 
-    print(f"Saved: {filename}")
+    post_links.append((title, filename))
+
+post_links.sort(key=lambda x: os.path.getctime(x[1]), reverse=True)
+
+index_html_content = """
+<html>
+    <head><title>Jadi Clone Blog</title></head>
+    <body>
+        <h1>Jadi Clone Blog</h1>
+        <h2>Recent Posts</h2>
+        <ul>
+"""
+
+for title, filename in post_links:
+    index_html_content += f'<li><a href="{filename}">{title}</a></li>\n'
+
+index_html_content += """
+        </ul>
+    </body>
+</html>
+"""
+
+with open(INDEX_FILE, 'w', encoding='utf-8') as f:
+    f.write(index_html_content)
+
+print("Posts and index.html updated.")
